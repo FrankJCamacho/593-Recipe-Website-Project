eaf03f481be039da6f151d78151815b3b7594c70
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2023-12-07 22:27:15 -0500

    Added a POST statement to function
    Fixed some variables and added a fetchData for register

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -7,11 +7,27 @@ function newRegister(e) {
     e.preventDefault()
 
     let user = {
-        firstName: document.getElementById("first").value,
-        lastName: document.getElementById("last").value,
-        userName: document.getElementById("username").value,
+        first: document.getElementById("first").value,
+        last: document.getElementById("last").value,
+        username: document.getElementById("username").value,
         password: document.getElementById("password").value
     }
+
+    fetchData("/user/register", user, "POST")
+    .then(data => {
+        if(!data.message) {
+            window.location.href = "post.html"
+        }
+    })
+
+    .catch(err => {
+        let errorSection = document.querySelector("#registerForm .error")
+        errorSection.innerText = err.message
+        document.getElementById("username").value = ""
+        document.getElementById("password").value = ""
+        document.getElementById("first").value = ""
+        document.getElementById("last").value = ""
+    })
 
     let h3 = document.getElementById("greetings")
 
