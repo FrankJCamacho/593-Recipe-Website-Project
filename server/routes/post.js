27203ced289534f345d0bdef2e597d50ef342460
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2023-12-07 22:21:56 -0500

    Added CRUD operations
    Create a post, read a post, edit a post and delete a post

--- server/routes/post.js
+++ server/routes/post.js
@@ -11,5 +11,44 @@ router.get('/getAllPosts', (req, res) => {
   }
 })
 
+//READ
+.post('/read', async (req, res) => {
+  try {
+    const post = await Post.readPost(req.body)
+    res.send({...post})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
+
+//CREATE
+.post('/create', async (req, res) => {
+  try {
+    const post = await Post.createPost(req.body)
+    res.send({...post})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
+
+//EDIT
+.put('/edit', async (req, res) => {
+  try {
+    let post = await Post.editPost(req.body)
+    res.send({...post})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
+
+//DELETE
+.delete('/delete', async (req, res) => {
+  try {
+    await Post.deletePost(req.body)
+    res.send({success: "Goodbye!"})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
 
 module.exports = router;
