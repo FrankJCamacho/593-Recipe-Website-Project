a3a834b252eaae2b3fdd22b531804debb7c38340
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2023-12-07 22:23:54 -0500

    SQL support added
    Create table function, functions for CRUD operations

--- server/models/post.js
+++ server/models/post.js
@@ -1,25 +1,97 @@
-const posts = [
-    {
-        title: "The Best Recipe!",
-        steps: "1. Love, 2. Hope",
-    },
-    {
-        title: "Chicken Sandwich",
-        steps: "1.Chicken, 2. Bread",
-    },
-    {
-        title: "Pizza",
-        steps: "1. Order Online!",
-    }
-  ]
+// const posts = [
+//     {
+//         title: "The Best Recipe!",
+//         steps: "1. Love, 2. Hope",
+//     },
+//     {
+//         title: "Chicken Sandwich",
+//         steps: "1.Chicken, 2. Bread",
+//     },
+//     {
+//         title: "Pizza",
+//         steps: "1. Order Online!",
+//     }
+//   ]
   
   // CRUD functions
   let getPosts = () => posts;
   
-  function getPosts2() {
-    return posts;
+//   function getPosts2() {
+//     return posts;
+//   }
+  
+const con = require("./db_connect");
+
+async function createTable() {
+    let sql = `CREATE TABLE IF NOT EXISTS post (
+        post_id INT NOT NULL AUTO_INCREMENT,
+        user_id INT NOT NULL,
+        post_title VARCHAR(255) NOT NULL,
+        post_description VARCHAR(255) NOT NULL,
+        CONSTRAINT post_pk PRIMARY KEY(post_id),
+        CONSTRAINT user_fk FOREIGN KEY(user_id) REFERENCES user(user_id)
+    )`;
+  
+    await con.query(sql)
+}
+  
+createTable()
+
+// CREATE - CRUD - C
+async function createPost(post) {
+    let postResult = await getUser(post.title)
+    if(postResult.length > 0) throw Error("Title already in use!! Please pick a different one.")
+
+    let sql = `
+      INSERT INTO post(post_title, post_description)
+      VALUES("${post.title}", "${post.description}")
+    `
+  
+    await con.query(sql)
+    const newPost = await getPost(post.title)
+    return newPost[0]
+}
+
+// READ - CRUD - R
+
+async function readPost(post) {
+    let postResult = await getPost(post.title)
+    if(!postResult[0]) throw Error("Post not found!! Please try another title.")
+  
+    return postResult[0]
   }
   
+// UPDATE - CRUD - U
+async function editPost(user) {
+    let updatedPost = await getPost(post.title)
+    if(updatedPost.length > 0) throw Error("Post not available!")
+  
+    let sql = `UPDATE post
+      SET post_title = "${post.title}"
+      WHERE post_description = ${post.description}
+    `
+    await con.query(sql)
+    updatedPost = await getPost(post.title)
+    return updatedPost[0]
+}
+  
+// DELETE - CRUD - D
+async function deletePost(post) {
+    let sql = `DELETE FROM post
+      WHERE post_id = ${post.PostId}
+    `
+    await con.query(sql)
+}
+
+// Useful functions
+async function getPost(title) {
+    let sql = `
+      SELECT * FROM post 
+      WHERE post_title = "${title}" 
+    `
+    return await con.query(sql)
+  }
+
   // export functions so can utilize them in another
   // file in application
-  module.exports = {getPosts, getPosts2}
+module.exports = {createPost, readPost, editPost, deletePost}
