5e149fe0f8c8dd61431b3d9f7143a67da0054aa8
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2023-12-07 22:20:54 -0500

    Added CRUD operations
    Create, Read, Update, and Delete operations added for the user (registering, logging in, editing a user, and deleting a user

--- server/routes/user.js
+++ server/routes/user.js
@@ -11,5 +11,44 @@ router.get('/getAllUsers', (req, res) => {
   }
 })
 
+//LOGIN
+.post('/login', async (req, res) => {
+  try {
+    const user = await User.login(req.body)
+    res.send({...user, password: undefined})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
+
+//REGISTER
+.post('/register', async (req, res) => {
+  try {
+    const user = await User.register(req.body)
+    res.send({...user, password: undefined})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
+
+//EDIT
+.put('/edit', async (req, res) => {
+  try {
+    let user = await User.editUser(req.body)
+    res.send({...user, password: undefined})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
+
+//DELETE
+.delete('/delete', async (req, res) => {
+  try {
+    await User.deleteUser(req.body)
+    res.send({success: "Goodbye!"})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
 
 module.exports = router;
