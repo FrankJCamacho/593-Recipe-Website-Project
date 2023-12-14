e47498c92b2f97170b0a9a575bb7adaf9afcf1f1
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2023-12-14 16:18:01 -0500

    Added main.js script

---
+++ public/scripts/main.js
@@ -0,0 +1,61 @@
+let nav = document.querySelector("nav")
+
+if(getCurrentUser()) {
+  nav.innerHTML = `
+     <ul>
+        <li><a href="post.html">Post</a></li>
+        <li><a href="profile.html">Profile</a></li>
+        <li><a id="logout">Logout</li>
+      </ul>
+  `
+} else {
+  nav.innerHTML = `
+     <ul>
+       <li><a href="post.html">Post</a></li>
+       <li><a href="profile.html">Profile</a></li>
+       <li><a href="login.html">Login</a></li>
+       <li><a href="register.html">Register</a></li>
+     </ul>
+  `
+}
+
+// Fetch method implementation:
+export async function fetchData(route = '', data = {}, methodType) {
+  const response = await fetch(`http://localhost:3000${route}`, {
+    method: methodType, // *POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data) 
+  });
+  if(response.ok) {
+    return await response.json(); 
+  } else {
+    throw await response.json();
+  }
+} 
+
+// LOCAL STORAGE FUNCTIONALITY
+export function setCurrentUser(user) {
+    localStorage.setItem('user', JSON.stringify(user))
+}
+
+export function getCurrentUser() {
+  return JSON.parse(localStorage.getItem('user'))
+}
+
+export function setPost(post) {
+    localStorage.setItem('post', JSON.stringify(post))
+}
+
+export function getPosts() {
+    return JSON.parse(localStorage.getItem('post'))
+}
+
+export function logout() {
+  localStorage.removeItem('user')
+  window.location.href = "login.html"
+}
+
+let logoutBtn = document.getElementById("logout")
+if(logoutBtn) logoutBtn.addEventListener("click", logout)
