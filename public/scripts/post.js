e86a72586ef8766453304efa8a55a6a15dd5c8fb
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2023-12-14 16:19:21 -0500

    Imported functions from main

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -1,3 +1,5 @@
+import { fetchData, setPost } from "./main"
+
 //Post Form
 let postForm = document.getElementById("postForm")
 postForm.addEventListener('submit', newPost)
@@ -9,9 +11,25 @@ function newPost(e) {
         title: document.getElementById("title").value,
         description: document.getElementById("text").value
     }
-    
-    let h3 = document.getElementById("recipe")
 
-    h3.innerHTML = `I'm sure, ${post.title} is very tasty!`
-    console.log(post.title, post.description) //testing
+    fetchData("/post/createPost", post, "POST")
+    .then(data => {
+        if(!data.message) {
+            console.log(data)
+            setPost(data)
+            window.location.href = "post.html"
+        }
+    })
+
+    .catch(err => {
+        let errorSection = document.querySelector("#post-form .error")
+        errorSection.innerText = err.message
+        document.getElementById("title").value = ""
+        document.getElementById("description").value = ""
+    })
+
+    // let h3 = document.getElementById("recipe")
+
+    // h3.innerHTML = `I'm sure, ${post.title} is very tasty!`
+    // console.log(post.title, post.description) //testing
 }
